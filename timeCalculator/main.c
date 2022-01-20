a9ed64438ffe26468ba9bc701d2bb38acb544dde
Baek Han <han.baek.program@gmail.com>
2022-01-19 18:47:57 -0800

    Create Menu

--- timeCalculator/main.c
+++ timeCalculator/main.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
 
+char resultTime[8] = "00:00";
+
+int showMenu() {
+    int menu;
+    
+    printf("# Current Time: %s\n", resultTime);
+    printf("# 1. Add time\n");
+    printf("# 2. Subtract time\n");
+    printf("# 3. Get result\n");
+    printf("# 0. Exit\n");
+    printf("#------------------------------------\n");
+    printf("# Enter: ");
+    scanf("%d", &menu);
+
+    if (menu < 0 || menu > 3) {
+        while (menu < 0 || menu > 3) {
+            printf("#\n");
+            printf("# Warning - Invalid option\n");
+            printf("# Re-enter: ");
+            scanf("%d", &menu);
+        }
+    }
+    
+    if (menu == 0) {
+        return 0;
+    } else if (menu == 1) {
+//        addTime();
+    } else if (menu == 2) {
+//        subtractTime();
+    } else if (menu == 3) {
+//        getResult();
+    }
+    
+    return 0;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    printf("########## Time Calculator ##########\n");
+    showMenu();
+    printf("#####################################\n");
     return 0;
 }
